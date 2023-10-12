862440432a1e2c62121645697231ab0dbe138bb1
ChongWeiJie29 <chongweijie29@gmail.com>
2023-10-12 21:06:09 +0800

    Update import lines to match checkstyle

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -15,7 +15,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TOTAL_STOCK;
 // import java.util.Collections;
 // import java.util.Optional;
 // import java.util.Set;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditPrescriptionDescriptor;
